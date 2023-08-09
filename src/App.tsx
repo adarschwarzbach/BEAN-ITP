7b693838fa9708219de9d2142ea09bf5eddc69b7
adarschwarzbach <adar.schwarzbach@gmail.com>
2023-08-08 19:40:48 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { InputGroup, Tooltip, Position, H5, Button, FormGroup, Intent, Switch, Card, Elevation, Divider, Classes } from '@blueprintjs/core';
 
-function App() {
+
+const App: React.FC = () => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(true);
+  const themeClass = isDarkMode ? 'bp5-dark' : '';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${themeClass} app-container `}>
+      <div className={`bp3-ui-text ${themeClass}`} style={{ padding: '20px' }}>
+          <H5>Input Details</H5>
+          <Divider />
+          <FormGroup
+              label="Name"
+              labelFor="name-input"
+              helperText={<Tooltip content="The name of the compound" position={Position.RIGHT}><span>?</span></Tooltip>}
+          >
+              <InputGroup id="name-input" placeholder="Enter name" />
+          </FormGroup>
+
+          <FormGroup
+              label="Valence"
+              labelFor="valence-input"
+              helperText={<Tooltip content="Valence electrons of the compound" position={Position.RIGHT}><span>?</span></Tooltip>}
+          >
+              <InputGroup id="valence-input" placeholder="Enter valence" type="number" />
+          </FormGroup>
+
+          <FormGroup
+              label="Mobility pKa"
+              labelFor="mobility-input"
+              helperText={<Tooltip content="Mobility pKa value" position={Position.RIGHT}><span>?</span></Tooltip>}
+          >
+              <InputGroup id="mobility-input" placeholder="Enter mobility pKa" type="number" />
+          </FormGroup>
+
+          <FormGroup
+              label="Concentration"
+              labelFor="concentration-input"
+              helperText={<Tooltip content="Concentration in the given solution" position={Position.RIGHT}><span>?</span></Tooltip>}
+          >
+              <InputGroup id="concentration-input" placeholder="Enter concentration" type="number" />
+          </FormGroup>
+
+          <FormGroup
+              label="Type"
+              labelFor="type-input"
+              helperText={<Tooltip content="Type of the compound" position={Position.RIGHT}><span>?</span></Tooltip>}
+          >
+              <InputGroup id="type-input" placeholder="Enter type" />
+          </FormGroup>
+
+          <Button intent={Intent.PRIMARY}>Submit</Button>
+          <Switch checked={isDarkMode} label="Dark Mode" onChange={() => setIsDarkMode(!isDarkMode)} style={{marginTop: '20px'}} />
+      </div>
     </div>
   );
 }
+
 
 export default App;
