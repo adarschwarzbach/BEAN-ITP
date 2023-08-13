628ca46346d08f59e32e44f7bf5c7238a9a70826
adarschwarzbach <adar.schwarzbach@gmail.com>
2023-08-12 21:18:17 -0700

    progress building first table

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import './App.css';
 import React, { useState } from 'react';
 import HeaderBanner from './components/Header/HeaderBanner';
 import SpeciesForm from './SpeciesData/SpeciesForm';
-
-
+import ZoneConcentrationsTable from './SpeciesData/SpeciesTables/ZoneConcentrationTable';
 const App: React.FC = () => {
 	const [isDarkMode, setIsDarkMode] = useState<boolean>(true);
 	const themeClass = isDarkMode ? 'bp5-dark' : '';
@@ -11,15 +10,18 @@ const App: React.FC = () => {
 	return (
 		<>
 			<HeaderBanner className={themeClass}/>
-			<div className={`${themeClass} app-container `}>
-				<div className={`bp3-ui-text ${themeClass}`} style={{ padding: '20px' }}>
-					<div className='form-data'>
-						<SpeciesForm />
-						<SpeciesForm />
-						<SpeciesForm />
-						<SpeciesForm />
-					</div>
+			<div className = {themeClass} style = {{display:'flex', alignItems:'center', marginRight:'12px'}}>
+				<div className={`${themeClass} app-container `}>
+					<div className={`bp3-ui-text ${themeClass}`} style={{ padding: '20px' }}>
+						<div className='form-data'>
+							<SpeciesForm />
+							<SpeciesForm />
+							<SpeciesForm />
+							<SpeciesForm />
+						</div>
+					</div>	
 				</div>
+				<ZoneConcentrationsTable/>
 			</div>
 		</>
 	);
---
+++ src/SpeciesData/SpeciesTables/ZoneConcentrationTable.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Column, Table2, Cell, Utils } from '@blueprintjs/table';
+import './ZoneConcentrationTable.css';
+
+type SpeciesData = {
+    species: string;
+    zone1: number;
+    zone2: number;
+    zone3: number;
+    zone4: number;
+};
+
+const computedZoneConcentrations: number[][] = [
+	[10.0, 0.0, 0.0, 0.0],
+	[20.0, 16.554550572712998, 16.09768634504494, 20.0],
+	[0.0, 6.540734950270157, 0.0, 0.0],
+	[0.0, 0.0, 6.077203891937898, 5.0]
+];
+
+const speciesList = ['SpeciesA', 'SpeciesB', 'SpeciesC', 'SpeciesD'];
+
+const data: SpeciesData[] = computedZoneConcentrations.map((row, index) => ({
+	species: speciesList[index],
+	zone1: row[0],
+	zone2: row[1],
+	zone3: row[2],
+	zone4: row[3],
+}));
+
+
+function truncateToThreeDecimal(num: number): number {
+	return Math.trunc(num * 1000) / 1000;
+}
+
+
+const columnNames = ['Species', 'Zone 1', 'Zone 2', 'Zone 3', 'Zone 4'];
+
+
+const ZoneConcentrationsTable: React.FC = () => {
+	const numRows = data.length;
+
+	const renderCell = (rowIndex: number, columnIndex: number) => {
+		if (columnIndex === 0) {
+			return <Cell>{`Species${rowIndex + 1}`}</Cell>; // Returns 'Species1', 'Species2', etc.
+		} else {
+			return <Cell>{truncateToThreeDecimal(computedZoneConcentrations[rowIndex][columnIndex - 1])}</Cell>;
+		}
+	};
+
+	const renderColumns = () => {
+		return columnNames.map((columnName, columnIndex) => {
+			return (
+				<Column
+					key={columnIndex}
+					name={columnName}
+					cellRenderer={(rowIndex: number) => renderCell(rowIndex, columnIndex)}
+				/>
+			);
+		});
+	};
+
+	return (
+		// <div className="table-container bp5-dark">
+		<Table2 
+			numRows={numRows}
+		>
+			{renderColumns()}
+		</Table2>
+		// </div>
+	);
+};
+
+export default ZoneConcentrationsTable;
