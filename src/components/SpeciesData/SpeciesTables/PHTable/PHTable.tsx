f54521d67ddf3eae1414796c0339464d1317f64e
adarschwarzbach <adar.schwarzbach@gmail.com>
2023-08-15 11:49:54 -0700

    hook up pH to global state

--- src/components/SpeciesData/SpeciesTables/PHTable/PHTable.tsx
+++ src/components/SpeciesData/SpeciesTables/PHTable/PHTable.tsx
@@ -3,26 +3,25 @@ import { Column, Table2, Cell, TableLoadingOption } from '@blueprintjs/table';
 import '../ZoneConcentrationTable.css';
 import { useSpeciesData } from '../../../../Contexts/SpeciesData';
 
-const concentrations = [
-	8.396585528437043e-09,
-	4.865719840824042e-09,
-	4.198872356494103e-09,
-	2.5311705295064293e-09
-];
 
-function toPHValue(concentration: number): number {
-	return -Math.log10(concentration);
-}
 
-const pHData = concentrations.map(toPHValue);
 
-const columnNames = ['Zone 1', 'Zone 2', 'Zone 3', 'Zone 4'];
 
 const PHTable: React.FC = () => {
-	const { loading } = useSpeciesData();
+	
+
+	const { loading, speciesDict, beanResults } = useSpeciesData();
 	const loadingOptions = loading ? [TableLoadingOption.CELLS] : [];
 
 	const numRows = 1; // We just have one row of pH values.
+
+	const columnNames = Object.values(speciesDict).map(species => species.Name);
+
+	function toPHValue(concentration: number): number {
+		return -Math.log10(concentration);
+	}
+	
+	const pHData = beanResults.cH[0].map(toPHValue);
 
 	const renderCell = (rowIndex: number, columnIndex: number) => {
 		return <Cell>{pHData[columnIndex].toFixed(3)}</Cell>;
@@ -41,10 +40,11 @@ const PHTable: React.FC = () => {
 	};
 
 	return (
-		<div className="ph-table-container">
+		<div style = {{width: 600}}>
 			<Table2 
 				numRows={numRows}
 				loadingOptions={loadingOptions}
+				enableRowHeader={false} // Add this line
 			>
 				{renderColumns()}
 			</Table2>
