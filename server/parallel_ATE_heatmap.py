4c59a8ed0b1e0e321bffa712ca11828deda3aa29
adarschwarzbach <adar.schwarzbach@gmail.com>
2023-11-04 17:14:45 -0400

    generate heatmap data neatly to start

--- server/parallel_ATE_heatmap.py
+++ server/parallel_ATE_heatmap.py
@@ -5,7 +5,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 
 client_config = botocore.config.Config(
-    max_pool_connections=100,
+    max_pool_connections=700,
 )
 
 lambda_client = boto3.client('lambda', config=client_config)
@@ -14,68 +14,13 @@ def invoke_worker_lambda(args):
     
     pH = args[1]
     LE_C = args[0]
+    species = args[2]
+    ionicEffect = args[3]
     payload =  {
-      "ionicEffect": 0,
+      "ionicEffect": ionicEffect,
       "pH": pH,
       "LE_C":LE_C,
-      "species": {
-        "0": {
-          "Name": "HCl",
-          "valence": [
-            -1
-          ],
-          "mobility": [
-            -7.91e-8
-          ],
-          "pKa": [
-            -2
-          ],
-          "concentration": 0.01,
-          "type": "LE"
-        },
-        "1": {
-          "Name": "Tris",
-          "valence": [
-            1
-          ],
-          "mobility": [
-            2.95e-8
-          ],
-          "pKa": [
-            8.076
-          ],
-          "concentration": 0.02,
-          "type": "Background"
-        },
-        "2": {
-          "Name": "MOPS",
-          "valence": [
-            -1
-          ],
-          "mobility": [
-            -2.69e-8
-          ],
-          "pKa": [
-            7.2
-          ],
-          "concentration": 0.001,
-          "type": "Analyte"
-        },
-        "3": {
-          "Name": "HEPES",
-          "valence": [
-            -1
-          ],
-          "mobility": [
-            -2.35e-8
-          ],
-          "pKa": [
-            7.5
-          ],
-          "concentration": 0.005,
-          "type": "TE"
-        }
-      }
+      "species":species
     }
 
 
@@ -92,13 +37,18 @@ def organize_results_into_grid(flat_results, num_rows, num_columns):
     return grid_results
 
 def lambda_handler(event, context):
+    # gets vars from context
+    species = event.get('species')
+    ionicEffect = event.get('ionicEffect')
+    pH = event.get('pH')
     # Generate 20 LE_C values between 1-1000
     LE_C_values = [i * 50 for i in range(1, 21)]
+    
 
     # Generate 20 pH values between 8.7 - 1 and 8.7 + 2
-    pH_values = [8.7 + i * 0.15 - 1 for i in range(20)]
+    pH_values = [pH - 2 + i * 0.2 for i in range(21)]
 
-    args_list = [(LE_C, pH) for LE_C in LE_C_values for pH in pH_values]
+    args_list = [(LE_C, pH, species, ionicEffect) for LE_C in LE_C_values for pH in pH_values]
 
     with ThreadPoolExecutor(max_workers=700) as executor:
         results = list(executor.map(invoke_worker_lambda, args_list))
