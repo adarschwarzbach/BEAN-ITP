d138e6cec60e40d592209031d9fc463d21b4a3ba
adarschwarzbach <adar.schwarzbach@gmail.com>
2024-02-19 18:19:23 -0500

    remove stable itp predicted check

--- src/components/HeatMap/ITPCheckIndicator.tsx
+++ src/components/HeatMap/ITPCheckIndicator.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ITPCheckIndicator = () => {
 	return (
-		<div style={{ display: 'flex', alignItems: 'center', marginTop: '20px', fontSize: '10px', lineHeight: '20px' }}>
+		<div style={{ display: 'flex', alignItems: 'center', marginTop: '20px', fontSize: '12px', lineHeight: '20px' }}>
 			<div style={{ width: '14px', height: '14px', backgroundColor: 'white', marginRight: '10px' }}></div>
 			<p style={{ margin: 0 }}>Indicates a condition where ITP checks are not satisfied (e.g., sample does not focus between LE and TE)</p>
 		</div>
